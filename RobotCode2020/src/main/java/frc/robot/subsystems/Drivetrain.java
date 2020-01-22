6dddda9ef91fcd6cde7c62ec01b757c3fb627719
LePlebClam <59941350+LePlebClam@users.noreply.github.com>
2020-01-22 15:51:52 -0800

    Created Drivetrain
    Drivetrain instance is called "RobotDrivebase"

---
+++ RobotCode2020/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+
+/**
+ * Add your docs here.
+ */
+public class Drivetrain extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  Spark leftMotor = new Spark(RobotMap.DRIVE_LEFT_PORT);
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
