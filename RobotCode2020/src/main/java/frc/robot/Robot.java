894a3b630292043ae2951517a6a6fa738dc80f15
LePlebClam <59941350+LePlebClam@users.noreply.github.com>
2020-01-25 11:23:38 -0800

    Imported Mailbox and Drivetrain in Robot.java

--- RobotCode2020/src/main/java/frc/robot/Robot.java
+++ RobotCode2020/src/main/java/frc/robot/Robot.java
@@ -15,6 +15,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 
+import frc.robot.subsystems.Mailbox;
+import frc.robot.subsystems.Drivetrain;
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
