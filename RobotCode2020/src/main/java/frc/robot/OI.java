421e2bc94716b0d14dc73caaf833dc2ac69f84b8
LePlebClam <59941350+LePlebClam@users.noreply.github.com>
2020-01-25 11:42:14 -0800

    Added joystick read

--- RobotCode2020/src/main/java/frc/robot/OI.java
+++ RobotCode2020/src/main/java/frc/robot/OI.java
@@ -7,6 +7,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
@@ -19,6 +20,7 @@ public class OI {
   // number it is.
   // Joystick stick = new Joystick(port);
   // Button button = new JoystickButton(stick, buttonNumber);
+  public Joystick controller = new Joystick(0);
 
   // There are a few additional built in buttons you can use. Additionally,
   // by subclassing Button you can create custom triggers and bind those to
@@ -39,4 +41,10 @@ public class OI {
   // Start the command when the button is released and let it run the command
   // until it is finished as determined by it's isFinished method.
   // button.whenReleased(new ExampleCommand());
+  public double getX() {
+    return controller.getRawAxis(0);
+  }
+  public double getY() {
+    return controller.getRawAxis(1);
+  }
 }
