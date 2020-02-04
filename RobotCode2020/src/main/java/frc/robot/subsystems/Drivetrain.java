f05a85c29ab2893904811809970b9c26c05e9bb2
x64-bit <40089761+x64-bit@users.noreply.github.com>
2020-02-03 16:37:24 -0800

    fixed coasting motor controller

--- RobotCode2020/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ RobotCode2020/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -26,19 +26,27 @@ import frc.robot.commands.Drive;
 /** Drivetrain class w/ limelight vision tracking */
 public class Drivetrain extends Subsystem {
   // Drivetrain
-  CANSparkMax leftMotor = new CANSparkMax(RobotMap.MOTOR_LEFT_ID, MotorType.kBrushless);
-  CANSparkMax rightMotor = new CANSparkMax(RobotMap.MOTOR_RIGHT_ID, MotorType.kBrushless);
-  DifferentialDrive dualDrive = new DifferentialDrive(leftMotor, rightMotor);
+  CANSparkMax leftBackMotor = new CANSparkMax(RobotMap.MOTOR_LEFT_BACK_ID, MotorType.kBrushless);
+  CANSparkMax rightBackMotor = new CANSparkMax(RobotMap.MOTOR_RIGHT_BACK_ID, MotorType.kBrushless);
+  CANSparkMax leftFrontMotor = new CANSparkMax(RobotMap.MOTOR_LEFT_FRONT_ID, MotorType.kBrushless);
+  CANSparkMax rightFrontMotor = new CANSparkMax(RobotMap.MOTOR_RIGHT_FRONT_ID, MotorType.kBrushless);
+  DifferentialDrive dualDrive = new DifferentialDrive(leftBackMotor, rightBackMotor);
 
   // limelight table to read offset value from
   NetworkTable limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
 
   // encoders
-  CANEncoder lEncoder = leftMotor.getEncoder();
-  CANEncoder rEncoder = rightMotor.getEncoder();
+  CANEncoder lEncoder = leftFrontMotor.getEncoder();
+  CANEncoder rEncoder = rightBackMotor.getEncoder();
 
   // average
   double average = 0.0;
+
+  public Drivetrain() {
+    // set motors to follow
+    leftFrontMotor.follow(leftBackMotor);
+    rightFrontMotor.follow(rightBackMotor);
+  }
 
   // Wrapper classes
   public void drive(double speed, double rotation) {
