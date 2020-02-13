2dceb34e028cacade5dc5879b40ba52a65e01e11
x64-bit <40089761+x64-bit@users.noreply.github.com>
2020-02-13 10:14:32 -0800

    refactored pid turn stuff a bit

---
+++ RobotCode2020/src/main/java/frc/robot/commands/RotateToLimelightTarget.java
@@ -0,0 +1,62 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import frc.robot.Robot;
+import frc.robot.utils.Limelight;
+
+public class RotateToLimelightTarget extends Command {
+  private double P = 0.04;
+  private double I = 0.0;
+  private double D = 0.0055;
+  private PIDController pid = new PIDController(P, I, D);
+  private double commandStartTime;
+  public RotateToLimelightTarget() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.drivetrain);
+    pid.setTolerance(0.05);
+    commandStartTime = Timer.getFPGATimestamp();
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    Robot.drivetrain.calibrate();
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    Robot.drivetrain.drive(0, pid.calculate(Limelight.returnHorizontalOffset(), 0));
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    if (Timer.getFPGATimestamp() > commandStartTime + 2) {
+      return true;
+    } else {
+      return pid.atSetpoint();
+    }
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
--- RobotCode2020/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ RobotCode2020/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -10,45 +10,78 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANEncoder;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+// PID
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.SPI;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import frc.robot.commands.Drive;
 
 /** Drivetrain class w/ limelight vision tracking */
 public class Drivetrain extends Subsystem {
-  // Drivetrain
-  CANSparkMax leftBackMotor = new CANSparkMax(RobotMap.MOTOR_LEFT_BACK_ID, MotorType.kBrushless);
-  CANSparkMax rightBackMotor = new CANSparkMax(RobotMap.MOTOR_RIGHT_BACK_ID, MotorType.kBrushless);
-  CANSparkMax leftFrontMotor = new CANSparkMax(RobotMap.MOTOR_LEFT_FRONT_ID, MotorType.kBrushless);
-  CANSparkMax rightFrontMotor =
-      new CANSparkMax(RobotMap.MOTOR_RIGHT_FRONT_ID, MotorType.kBrushless);
-  DifferentialDrive dualDrive = new DifferentialDrive(leftBackMotor, rightBackMotor);
+  // motor stuff
+  CANSparkMax leftMotorFront = new CANSparkMax(RobotMap.MOTOR_LEFT_FRONT_ID, MotorType.kBrushless);
+  CANSparkMax leftMotorBack = new CANSparkMax(RobotMap.MOTOR_LEFT_BACK_ID, MotorType.kBrushless);
+  CANSparkMax rightMotorFront = new CANSparkMax(RobotMap.MOTOR_RIGHT_FRONT_ID, MotorType.kBrushless);
+  CANSparkMax rightMotorBack = new CANSparkMax(RobotMap.MOTOR_RIGHT_BACK_ID, MotorType.kBrushless);
+  SpeedControllerGroup leftMotors = new SpeedControllerGroup(leftMotorFront, leftMotorBack);
+  SpeedControllerGroup rightMotors = new SpeedControllerGroup(rightMotorFront, rightMotorBack);
 
-  // encoders
-  CANEncoder lEncoder = leftBackMotor.getEncoder();
-  CANEncoder rEncoder = rightBackMotor.getEncoder();
+  // drivetrain
+  DifferentialDrive dualDrive = new DifferentialDrive(leftMotors, rightMotors);
 
-  // average
-  double average = 0.0;
+  // gyro
+  // TO-DO: consider moving gyro to "utils"
+  public AHRS gyro;
 
-  // Wrapper classes
+  public Drivetrain() {
+    try {
+      gyro = new AHRS(SPI.Port.kMXP);
+    } catch (RuntimeException ex) {
+      DriverStation.reportError("Error instantiating navX MXP", true);
+    }
+  }
+
+  /**
+   * For outputting values to SmartDashboard or odometry
+   */
+  public void periodic() {
+    SmartDashboard.putNumber("gyro", gyro.getAngle());
+    // yeah
+  }
+
+  /**
+   * Manual calibration of gyro
+   */
+  public void calibrate() {
+    gyro.zeroYaw();
+  }
+
+  /**
+   * Wrapper for DifferentialDrive.arcadeDrive();
+   * @param speed [-1,1] speed forwards
+   * @param rotation [-1,1] rotation, negative is left
+   */
   public void drive(double speed, double rotation) {
     dualDrive.arcadeDrive(speed, rotation);
   }
 
+  /**
+   * Wrapper for DifferentialDrive.tankDrive();
+   * @param leftSpeed [-1,1] left wheel speed
+   * @param rightSpeed [-1,1] right wheel speed
+   */
   public void oldDrive(double leftSpeed, double rightSpeed) {
     dualDrive.tankDrive(leftSpeed, rightSpeed);
-  }
-
-  // TO-DO: Consider moving non-driving stuff to commands
-  // getters
-  public double getPosition() {
-    average = (lEncoder.getPosition() + rEncoder.getPosition()) / 2.0;
-    return average;
   }
 
   @Override
