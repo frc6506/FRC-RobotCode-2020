5583abada75cc8968f59862be873048a53f17978
BobSaidHi <1019928@student.salinasuhsd.org>
2020-02-19 16:25:18 -0800

    fixed arm.java- changed which improts werer ocmented out

--- RobotCode2020/src/main/java/frc/robot/subsystems/Arm.java
+++ RobotCode2020/src/main/java/frc/robot/subsystems/Arm.java
@@ -8,12 +8,11 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-// import frc.robot.RobotMap;
+import frc.robot.RobotMap;
 
-// import com.ctre.phoenix.motorcontrol.ControlMode;
-// import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import edu.wpi.first.wpilibj.controller.PIDController;
-
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+//import edu.wpi.first.wpilibj.controller.PIDController;
 import frc.robot.commands.ArmSet;
 
 /** Add your docs here. */
